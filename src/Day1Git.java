f32b17bab273b6a4df8592a5ff3b3716224a9c2f
Osman Kılıçaslan <tr.thematrix@gmail.com>
2022-08-15 16:20:10 +0300

    V1

---
+++ src/Day1Git.java
@@ -0,0 +1,30 @@
+public class Day1Git {
+    public static void main(String[] args) {
+        System.out.println("Hello Git 1. Deneme");
+        System.out.println("Hello Git 2. Deneme");
+        System.out.println("Hello Git 3. Deneme");
+        System.out.println("Hello Git 4. Deneme");
+        System.out.println("Hello Git 5. Deneme");
+
+        /*
+        --------Komutlar
+        git --version
+        git config --global user.email "email_adresiniz"
+        git config --global user.name "isminiz"
+        git config --global -l   --> Ayarları listeler
+        git init            --> git ile ilişkilendirir
+        git status          --> Working Space ve Staged deki değişiklikleri gösterir
+        git add .           --> Working'den staging area'ya gönderir
+        git status
+        git diff            --> Working space deki değişikliği gösterir
+        git diff --staged   --> Staging Area daki değişiklikleri gösterir
+        git commit -m "first commit"
+        git show "hashcode"    --> Versiyondaki değişiklikleri gösterir
+        git log --oneline
+        git remote add origin https://github.com/techproed2020/Git_...
+        git push -u origin master
+         */
+    }
+}
+
+
